d35bb59c3403d92435195ae5762b3c46286db07d
SaiMohan <saimohan.hyle@thundersoft.com>
2022-02-01 22:53:29 +0530

    modified

--- 2-datatypes.c
+++ 2-datatypes.c
@@ -1,23 +1,33 @@
 //Program to Explain all data types including signed and unsigned.
 
-#include<stdio.h>
-#include<limits.h>
-int main() 
-{
 
-    printf("SCHAR_MAX:%d\n", SCHAR_MAX);
-    printf("SCHAR_MIN:%d\n", SCHAR_MIN);
-    printf("SHRT_MAX:%d\n", SHRT_MAX);
-    printf("SHRT_MIN:%d\n", SHRT_MIN);	
-    printf("CHAR_MAX:%d\n", CHAR_MAX);
-    printf("CHAR_MIN:%d\n", CHAR_MIN);
-    printf("INT_MAX:%d\n", INT_MAX);
-    printf("INT_MIN:%d\n", INT_MIN);
-    printf("LONG_MAX:%ld\n", (long) LONG_MAX);
-    printf("LONG_MIN:%ld\n", (long) LONG_MIN);
-    printf("UCHAR_MAX:%d\n", UCHAR_MAX);
-    printf("UINT_MAX:%u\n", (unsigned int) UINT_MAX);
-    printf("ULONG_MAX:%lu\n", (unsigned long) ULONG_MAX);
-    printf("USHRT_MAX:%d\n", (unsigned short) USHRT_MAX);
-return 0;
-}
+
+ #include<stdio.h>
+   int main()
+   {
+           int a=1;
+           unsigned int b=2;
+           short signed int c=3;
+           short unsigned int d=4;
+           long signed int e=5;
+           long unsigned int f=6;
+          signed char ch='a';
+          unsigned char ch1 ='b';
+          float g=1.2;
+          double h=12.0;
+          long double i=13.12;
+  
+          printf("integer and size is             :       %d\t%ld\n",a,sizeof(a));
+          printf("unsigned int and size is        :       %u\t%ld\n",b,sizeof(b));
+          printf("short signed int and size is    :       %hi\t%ld\n",c,sizeof(c));
+          printf("short unsigned int and size is  :       %hu\t%ld\n",d,sizeof(d));
+          printf("long signed int and size is     :       %li\t%ld\n",e,sizeof(e));
+          printf("long unsigned int and size is   :       %lu\t%ld\n",f,sizeof(f));
+          printf("signed char and size is         :       %c\t%ld\n",ch,sizeof(ch));
+          printf("unsigned char and size is       :       %c\t%ld\n",ch1,sizeof(ch1));
+          printf("float and size is               :       %f\t%ld\n",g,sizeof(g));
+          printf("double and size is              :       %lf\t%ld\n",h,sizeof(h));
+          printf("long double and size is         :       %Lf\t%ld\n",i,sizeof(i));
+          return 0;
+  }
+
