46420d1881974bda41fcbc0b48acbe248cd73ac8
SaiMohan <saimohan.hyle@thundersoft.com>
2022-02-01 20:54:26 +0530

    submitted assignemnts

---
+++ 3-operators.c
@@ -0,0 +1,13 @@
+//Program to explain all opeartors using different data types.
+#include<stdio.h>
+int main()
+{
+	int a=20;
+	float b=12.3;reuslt=0;
+	printf("Addition=%f\n",a+b);
+	printf("Sub=%f\n",a-b);
+	printf("Mul=%f\n",a*b);
+	printf("Div=%f\n",a/b);
+	return 0;
+		
+}
