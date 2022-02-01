e23904686c6a0f4f0246167002bede7221f960ae
SaiMohan <saimohan.hyle@thundersoft.com>
2022-02-01 19:58:18 +0530

    1st commit

---
+++ 1-area_circumference_of_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define pie 3.14
+int main()
+{
+	float area=0,circum=0,radius;
+	printf("enter the radius\n");
+	scanf("%f",&radius);
+	circum = 2*pie*radius;
+	area =  pie*radius*radius;
+	printf("circum=%f\narea=%f\n",circum,area);
+	return 0;
+}
